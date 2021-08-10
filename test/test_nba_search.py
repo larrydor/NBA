cc1abad19bea5e09a31b6270e6f7d4bd7917c440
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-10 19:41:20 -0400

    Update test_nba_search.py

--- test/test_nba_search.py
+++ test/test_nba_search.py
@@ -3,4 +3,6 @@ from app.nba_search import fetch_roster
 def test_home():
     results = fetch_roster("Bulls")
     assert isinstance(results , list)
+    player = results[0]
+    assert isinstance(player, dict)
     
